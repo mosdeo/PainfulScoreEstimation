43f1b217e267116a2a52810472fc2d16b459086b
Lin, Kao-Yuan <mosdeo@gmail.com>
2018-05-31 22:20:14 +0800

    加入直接在影像上畫 face rect 和 PSPI 整數視覺化的 putText

--- Inference/Program(maxPooling).cs
+++ Inference/Program(maxPooling).cs
@@ -39,69 +39,69 @@ namespace Inference
 
                 //偵測人臉, 限定尺寸下限, 加快速度&過濾錯誤
                 swTest.Restart();
-                System.Drawing.Rectangle[] faceRect = faceDetect.DetectMultiScale(RawImg, 1.1, 3, new System.Drawing.Size(128, 128));
+                System.Drawing.Rectangle[] faceRect = faceDetect.DetectMultiScale(RawImg, 1.1, 10, new System.Drawing.Size(70, 70));
                 testTick = swTest.ElapsedMilliseconds;
-                //如果沒偵測到人臉
-                if (0 == faceRect.Length)
+                //如果偵測到人臉
+                if (0 != faceRect.Length)
                 {
-                    CvInvoke.Imshow(nameof(RawImg), RawImg);
-                }
-                else
-                {
-                    //取出待測的臉部ROI
-                    Mat faceImg = new Mat(RawImg, faceRect[0]);
-                    CvInvoke.Resize(faceImg, faceImg, new System.Drawing.Size(64, 64),0,0);
-                    CvInvoke.CvtColor(faceImg, faceImg, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
-
-                    //原始灰階+直方圖等化
-                    CvInvoke.Imshow("Bgr2Gray", faceImg);
-                    CvInvoke.EqualizeHist(faceImg, faceImg);
-
-                    ////Flip
-                    //Mat flipFace = new Mat();
-                    //CvInvoke.Flip(faceImg, flipFace, Emgu.CV.CvEnum.FlipType.Horizontal);
-                    //CvInvoke.Imshow("FlipH", flipFace);
-
-                    ////Equalize
-                    //Mat equFace = new Mat();
-                    //CvInvoke.EqualizeHist(faceImg, equFace);
-                    //CvInvoke.Imshow("HistEqu", equFace);
-
-                    //CvInvoke.EqualizeHist(faceImg, faceImg);
-                    //CvInvoke.Flip(faceImg, faceImg, Emgu.CV.CvEnum.FlipType.Horizontal);
-                    //CvInvoke.Imshow("HistEqu+FlipH", faceImg);
-
-                    //Max Pooling
-                    double[] inputVector;
-                    
-                    Mat PoolingImg = Pooling.Do(faceImg, true, out inputVector);
-                    //使用預設的平均值和標準差,做高斯正規化
-                    
-                    for (int i = 0; i < 1024; i++)
-                    //Parallel.For(0, 1024, i =>
+                    for (int face = 0; face < faceRect.Length; face++)
                     {
-                        inputVector[i] =(inputVector[i] - WeightVector.PainFace_1024_DA_Avg[i]) / WeightVector.PainFace_1024_DA_StdErr[i];
-                    }
-                    //);
-                    
-                    double NN_Estimation_PSPI = nn.ComputeOutputs(inputVector)[0];   
-                    Console.Clear();
-                    CvInvoke.Imshow(nameof(RawImg), RawImg);
-                    CvInvoke.Imshow(nameof(faceImg), faceImg);
-                    int scalePoolImg = 6;
-                    CvInvoke.Resize(PoolingImg, PoolingImg, new System.Drawing.Size(scalePoolImg*PoolingImg.Cols, scalePoolImg * PoolingImg.Rows), 0, 0, Emgu.CV.CvEnum.Inter.Nearest);
-                    CvInvoke.Imshow(nameof(PoolingImg), PoolingImg);
-                    Console.WriteLine(nameof(testTick)+"={0:0.00}", testTick);
-                    Console.WriteLine("FPS={0:0.00}", 1000/aLoopTime);
-                    Console.WriteLine("{0} = {1:0.00}", nameof(NN_Estimation_PSPI), NN_Estimation_PSPI);
-                    Console.Write("Pain = "); for (int i = 0; i < NN_Estimation_PSPI; i++) { Console.Write("▉"); }
+                        //取出待測的臉部ROI
+                        Mat faceImg = new Mat(RawImg, faceRect[face]);
+                        CvInvoke.Resize(faceImg, faceImg, new System.Drawing.Size(64, 64), 0, 0);
+                        CvInvoke.CvtColor(faceImg, faceImg, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                        CvInvoke.EqualizeHist(faceImg, faceImg);
 
-                    //存一些範例
-                    //CvInvoke.Imwrite(nameof(faceImg)+".png", faceImg);
-                    //CvInvoke.Imwrite(nameof(PoolingImg)+".png", PoolingImg);
+                        //Max Pooling
+                        double[] inputVector;
+
+                        Mat PoolingImg = Pooling.Do(faceImg, true, out inputVector);
+                        //使用預設的平均值和標準差,做高斯正規化
+
+                        for (int i = 0; i < 1024; i++)
+                        //Parallel.For(0, 1024, i =>
+                        {
+                            inputVector[i] = (inputVector[i] - WeightVector.PainFace_1024_DA_Avg[i]) / WeightVector.PainFace_1024_DA_StdErr[i];
+                        }
+                        //);
+
+                        double NN_Estimation_PSPI = nn.ComputeOutputs(inputVector)[0];
+
+                        DrawFaceBox(RawImg, faceRect[face], NN_Estimation_PSPI);
+
+                        //if (NN_Estimation_PSPI > MaxPSPI)
+                        //{
+                        //    CvInvoke.Imwrite("PSPI=" + Convert.ToInt32(NN_Estimation_PSPI).ToString() + ".png", RawImg);
+                        //    MaxPSPI = NN_Estimation_PSPI;
+                        //}
+                    }
                 }
+
+                //寫入畫面上的文字訊息
+                String fps = String.Format("FPS={0:0.00}", 1000 / aLoopTime);
+                CvInvoke.PutText(RawImg, fps, new System.Drawing.Point(0, 30), 0, 1, new Emgu.CV.Structure.MCvScalar(51, 150, 255), 1);
+
+                CvInvoke.Imshow(nameof(RawImg), RawImg);
+                //vw.Write(RawImg);
                 CvInvoke.WaitKey(1);
             }
+        }
+
+        static void DrawFaceBox(Mat frameFaceBox, System.Drawing.Rectangle faceRect, double PSPI)
+        {
+            Emgu.CV.Structure.MCvScalar colorOrange = new Emgu.CV.Structure.MCvScalar(51, 150, 255);
+            System.Drawing.Point textPosition = new System.Drawing.Point(faceRect.X, faceRect.Y + 22 + faceRect.Size.Height);
+
+            String painNum = String.Format("PSPI={0:0.0}", PSPI);
+            String painBar = "";
+            for (int i = 0; i < PSPI; i++) { painBar += "*"; }
+            //Console.Write("Pain = "); for (int i = 0; i < NN_Estimation_PSPI; i++) { Console.Write("▉"); }
+
+            CvInvoke.Rectangle(frameFaceBox, faceRect, colorOrange, 1);
+            CvInvoke.PutText(frameFaceBox, painNum, textPosition, Emgu.CV.CvEnum.FontFace.HersheyComplex, 1, colorOrange, 2);
+
+            textPosition.Y = faceRect.Y;
+            CvInvoke.PutText(frameFaceBox, painBar, textPosition, Emgu.CV.CvEnum.FontFace.HersheyComplex, 1, colorOrange, 2);
         }
     }
 }
