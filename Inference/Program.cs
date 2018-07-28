3df36fd063c1a7856192d932211a17c7cc42e2e2
Lin Kao Yuan <mosdeo@gmail.com>
2018-07-29 07:12:36 +0800

    小幅度優化

--- Inference/Program.cs
+++ Inference/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Emgu.CV;
-using LKY;
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -20,7 +19,7 @@ namespace Inference
             ShowWindow(GetConsoleWindow(), 0);
 
             //初始化類神經網路
-            var painfulFaceScoreEstimator = new PainfulFaceScoreEstimator();
+            var painfulFaceScoreEstimator = new LKY.PainfulFaceScoreEstimator();
 
             //初始化攝影機與人臉偵測
             Emgu.CV.VideoCapture webCam = new VideoCapture();
@@ -39,21 +38,21 @@ namespace Inference
 
                 //偵測人臉, 限定尺寸下限, 加快速度&過濾錯誤
                 swTest.Restart();
-                System.Drawing.Rectangle[] faceRect = faceDetect.DetectMultiScale(RawImg, 1.1, 10, new System.Drawing.Size(70, 70));
+                System.Drawing.Rectangle[] faceRectArray = faceDetect.DetectMultiScale(RawImg, 1.1, 10, new System.Drawing.Size(70, 70));
                 testTick = swTest.ElapsedMilliseconds;
                 //如果偵測到人臉
-                if (0 != faceRect.Length)
+                if (0 != faceRectArray.Length)
                 {
-                    for (int face = 0; face < faceRect.Length; face++)
+                    foreach (var faceRect in faceRectArray)
                     {
                         //取出待測的臉部ROI
-                        Mat faceImg = new Mat(RawImg, faceRect[face]);
+                        Mat faceImg = new Mat(RawImg, faceRect);
                         CvInvoke.Resize(faceImg, faceImg, new System.Drawing.Size(64, 64), 0, 0);
                         CvInvoke.CvtColor(faceImg, faceImg, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
                         CvInvoke.EqualizeHist(faceImg, faceImg);
 
                         double NN_Estimation_PSPI = painfulFaceScoreEstimator.getInferencePSPIScore(faceImg);
-                        DrawFaceBox(RawImg, faceRect[face], NN_Estimation_PSPI);
+                        DrawFaceBox(RawImg, faceRect, NN_Estimation_PSPI);
                     }
                 }
 
@@ -61,8 +60,8 @@ namespace Inference
                 String fps = String.Format("FPS={0:0.00}", 1000 / aLoopTime);
                 CvInvoke.PutText(RawImg, fps, new System.Drawing.Point(0, 30), 0, 1, new Emgu.CV.Structure.MCvScalar(51, 150, 255), 1);
 
-                CvInvoke.Imshow(nameof(RawImg), RawImg);
-                //vw.Write(RawImg);
+                const string strWindowName = "Prkachin and Solomon Pain Intensity (PSPI) Estimator";
+                CvInvoke.Imshow(strWindowName, RawImg);
 
                 if(27 == CvInvoke.WaitKey(1))
                 {
@@ -79,15 +78,13 @@ namespace Inference
             System.Drawing.Point textPosition = new System.Drawing.Point(faceRect.X, faceRect.Y + 22 + faceRect.Size.Height);
 
             String painNum = String.Format("PSPI={0:0.0}", PSPI);
-            String painBar = "";
-            for (int i = 0; i < PSPI; i++) { painBar += "*"; }
-            //Console.Write("Pain = "); for (int i = 0; i < NN_Estimation_PSPI; i++) { Console.Write("▉"); }
+            String painLength = new string('*', Convert.ToInt32( PSPI>=0 ? PSPI : 0 ));
 
             CvInvoke.Rectangle(frameFaceBox, faceRect, colorOrange, 1);
             CvInvoke.PutText(frameFaceBox, painNum, textPosition, Emgu.CV.CvEnum.FontFace.HersheyComplex, 1, colorOrange, 2);
 
             textPosition.Y = faceRect.Y;
-            CvInvoke.PutText(frameFaceBox, painBar, textPosition, Emgu.CV.CvEnum.FontFace.HersheyComplex, 1, colorOrange, 2);
+            CvInvoke.PutText(frameFaceBox, painLength, textPosition, Emgu.CV.CvEnum.FontFace.HersheyComplex, 1, colorOrange, 2);
         }
     }
 }
